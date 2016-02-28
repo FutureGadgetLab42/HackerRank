a39123b50e4de938dfc3f1fc2b3c937fe17b9d5d
FutureGadgetLab42 <futuregadgetlab42@gmail.com>
2016-02-28 12:50:21 -0500

    I like cats, dogs and other furry buddies =)

--- src/DataStructures/Stacks/SimpleTextEditor/Solution.java
+++ src/DataStructures/Stacks/SimpleTextEditor/Solution.java
@@ -9,9 +9,24 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numOperations = Integer.parseInt(sc.nextLine());
-        for(int i = 0; i < numOperations; i++) {
+        int numOperations = Integer.parseInt(sc.nextLine()), commands[] = new int[numOperations];
+        String line[], parameters[] = new String[numOperations];
 
+        for(int i = 0; i < numOperations; i++) {
+            line = sc.nextLine().split(" ");
+            commands[i] = Integer.parseInt(line[0]);
+
+            if(line.length > 1) {
+                parameters[i] = line[1];
+            }
         }
+
+        for(int i = 0; i < numOperations; i++) {
+            System.out.println(process(commands, parameters));
+        }
+    }
+
+    private static String process(int[] commands, String[] parameters) {
+        return "";
     }
 }
