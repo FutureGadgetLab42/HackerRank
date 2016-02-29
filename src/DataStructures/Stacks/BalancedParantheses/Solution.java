038386a1f541da7f7ca759e47fb5b38796b09c78
FutureGadgetLab42 <futuregadgetlab42@gmail.com>
2016-02-28 22:34:43 -0500

    Added tests

--- src/DataStructures/Stacks/BalancedParantheses/Solution.java
+++ src/DataStructures/Stacks/BalancedParantheses/Solution.java
@@ -1,7 +1,23 @@
 package DataStructures.Stacks.BalancedParantheses;
 
+import java.util.Scanner;
+
 /**
  * https://www.hackerrank.com/challenges/balanced-parentheses
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numTestCases = Integer.parseInt(sc.nextLine());
+        String line;
+        for(int i = 0; i < numTestCases; i++) {
+            line = sc.nextLine();
+            System.out.println(isBalanced(line));
+        }
+    }
+
+    private static String isBalanced(String line) {
+        return "";
+    }
 }
