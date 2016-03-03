3f8002d6ee0e2f1ec20b1789b8124e2a1ab15bd3
FutureGadgetLab42 <futuregadgetlab42@gmail.com>
2016-03-03 18:51:25 -0500

    incomplete challenge sherlock and anagrams

--- src/Algorithms/Strings/SherlockAndAnagrams/Solution.java
+++ src/Algorithms/Strings/SherlockAndAnagrams/Solution.java
@@ -1,14 +1,26 @@
 package Algorithms.Strings.SherlockAndAnagrams;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
  * https://www.hackerrank.com/challenges/sherlock-and-anagrams
  */
 public class Solution {
+    private static final boolean LOCAL_TEST = true;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        if(LOCAL_TEST) {
+            try {
+                sc = new Scanner(new File("/Users/Tom/IdeaProjects/HackerRank and Miscellaneous/src/Algorithms/Strings/SherlockAndAnagrams/tests/input00"));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
         int numTestCases = Integer.parseInt(sc.nextLine());
         String input;
 
@@ -19,6 +31,31 @@ public class Solution {
     }
 
     private static int numAnagramPairs(String input) {
-        return 0;
+        int result = 0;
+        String currentSubstring;
+        HashMap<String, HashMap<Character, Integer>> subStringOccurrences = new HashMap<>();
+        HashMap<Character, Integer> currentMap;
+        for(int i = 0; i < input.length(); i++) {
+            for(int j = i; j < input.length(); j++) {
+                currentSubstring = input.substring(i, j+1);
+                if(subStringOccurrences.get(currentSubstring) == null) {
+                    subStringOccurrences.put(currentSubstring, new HashMap<>());
+                }
+                currentMap = subStringOccurrences.get(currentSubstring);
+                for(int k = 0; k < currentSubstring.length(); k++) {
+                    if(currentMap.get(currentSubstring.charAt(k)) == null) {
+                        currentMap.put(currentSubstring.charAt(k), 1);
+                    } else {
+                        currentMap.put(currentSubstring.charAt(k), currentMap.get(currentSubstring) +1);
+                    }
+                }
+            }
+        }
+
+        for(String substr : subStringOccurrences.keySet()) {
+//            currentMap = subStringOccurrences.get()
+        }
+
+        return result;
     }
 }
